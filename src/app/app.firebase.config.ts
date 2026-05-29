9782a0596d4a5fe7cd999676c2171176c221bbca
Sneha K B <snehakb221@gmail.com>
2026-05-29 23:02:39 +0530

    Angular project upload

--- src/app/app.firebase.config.ts
+++ src/app/app.firebase.config.ts
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAdENYvQg1gI_1pONInILcbQNX_Ji4Bss8',
+  apiKey: 'api_key',
   authDomain: 'expense-tracker-e0028.firebaseapp.com',
   databaseURL: 'https://expense-tracker-e0028.firebaseio.com',
   projectId: 'expense-tracker-e0028',
